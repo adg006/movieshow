22ad94399f0552ab9d7e07e6a73f08ef693ce48a
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-06-25 16:57:29 +0530

    Refactor App component to include routing and Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favourite from "./pages/Favourite";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
