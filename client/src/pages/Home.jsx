69f9aa079d1337eccc1623ac80bb27b3b687d59d
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-07-05 12:54:30 +0530

    Add Feature component to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Feature from "../components/Feature";
 import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Feature />
     </>
   );
 };
