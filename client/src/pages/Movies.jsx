3d619fe8fe36ee218b2a7c39dcd92ada31aab866
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-07-05 17:23:37 +0530

    Implement Movies component to display a list of available movies or a message when none are available

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { dummyShowsData } from "../assets/assets";
+import BlurBG from "../components/BlurBG";
+import Card from "../components/Card";
 
 const Movies = () => {
-  return <div></div>;
+  return dummyShowsData.length > 0 ? (
+    <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
+      <BlurBG top="150px" left="0" />
+      <BlurBG bottom="50px" right="50px" />
+      <h1 className="text-lg font-medium my-4">Now showing</h1>
+
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.map((movie) => (
+          <Card key={movie._id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-3xl font-bold text-center">No movies available</h2>
+    </div>
+  );
 };
 
 export default Movies;
