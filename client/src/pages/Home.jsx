a0b3497ed6d7978586265b2748ea44e2a20aaf84
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-07-05 16:29:03 +0530

    Add Trailer component to display video trailers

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Feature from "../components/Feature";
 import Hero from "../components/Hero";
+import Trailer from "../components/Trailer";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Feature />
+      <Trailer />
     </>
   );
 };
