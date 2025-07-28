9b4a282c00fda131066402a747d1331aecfba166
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-07-28 17:07:34 +0530

    Enhance SeatLayout with seat selection logic and UI improvements

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { ClockIcon } from "lucide-react";
-import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
+import { ArrowRightIcon, ClockIcon } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { assets, dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import isoTimeFormat from "../libs/isoTimeFormat.js";
 import Loading from "../components/Loading";
+import BlurBG from "../components/BlurBG.jsx";
 
 const SeatLayout = () => {
   const { id, date } = useParams();
   const [selectedSeats, setSelectedSeats] = useState([]);
   const [selectedTime, setSelectedTime] = useState(null);
   const [show, setShow] = useState(null);
+  const groupRows = [
+    ["A", "B"],
+    ["C", "D"],
+    ["E", "F"],
+    ["G", "H"],
+    ["I", "J"],
+  ];
 
   const navigate = useNavigate();
 
@@ -27,26 +36,95 @@ const SeatLayout = () => {
     getShow();
   }, []);
 
+  const handleSeatClick = (seatId) => {
+    if (!selectedTime) {
+      toast.error("Please select a time first.");
+      return;
+    }
+
+    if (!selectedSeats.includes(seatId) && selectedSeats.length > 4) {
+      toast.error("You can only select up to 5 seats.");
+      return;
+    }
+
+    setSelectedSeats((prev) =>
+      prev.includes(seatId)
+        ? prev.filter((seat) => seat !== seatId)
+        : [...prev, seatId]
+    );
+  };
+
+  const renderSeats = (row, count = 9) => (
+    <div className="flex gap-2 mt-2" key={row}>
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        {Array.from({ length: count }, (_, i) => {
+          const seatId = `${row}${i + 1}`;
+
+          return (
+            <button
+              className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${
+                selectedSeats.includes(seatId) && "bg-primary text-white"
+              } `}
+              key={seatId}
+              onClick={() => handleSeatClick(seatId)}
+            >
+              {seatId}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+
   return show ? (
     <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:py-50">
       <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30">
         <p className="text-lg font-semibold px-6">Avalilable Timings</p>
 
         <div className="mt-5 space-y-1">
-          {show.dateTime[date].map((item, index) => (
+          {show.dateTime[date].map((item) => (
             <div
               className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${
                 selectedTime?.time === item.time
                   ? "bg-primary text-white"
                   : "hover:bg-primary/20"
               }`}
-              key={index}
+              key={item.time}
+              onClick={() => setSelectedTime(item)}
             >
               <ClockIcon className="w-4 h-4" />
               <p className="text-sm">{isoTimeFormat(item.time)}</p>
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="relative flex-1 flex flex-col items-center max-md:mt-16">
+        <BlurBG top="-100px" left="-100px" />
+        <BlurBG bottom="0" right="0" />
+        <h1 className="text-2xl font-semibold mb-4">Select your seat</h1>
+        <img src={assets.screenImage} alt="screen" />
+        <p className="text-gray-400 text-sm mb-6 uppercase">Screen Side</p>
+
+        <div className="flex flex-col items-center mt-10 text-xs text-gray-300 ">
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6">
+            {groupRows[0].map((row) => renderSeats(row))}
+          </div>
+
+          <div className="grid grid-cols-2 gap-11">
+            {groupRows.slice(1).map((group, index) => (
+              <div key={index}>{group.map((row) => renderSeats(row))}</div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          className="flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95"
+          onClick={() => navigate("/my-bookings")}
+        >
+          Proceed to checkout{" "}
+          <ArrowRightIcon className="w-4 h-4" strokeWidth={3} />
+        </button>
       </div>
     </div>
   ) : (
