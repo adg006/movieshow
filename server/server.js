773b2dd85abdd1fbc28a7dc9df94d84e935b447b
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-08-01 12:03:50 +0530

    Add database connection setup and update .gitignore to include .env

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+await connectDB();
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Movie Show API");
