a600a7155e37092c372c19efa37cb65a12964043
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-08-01 12:20:18 +0530

    Add user model

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    mongoose.connection.on("connected", () =>
+      console.log("MongoDB connected successfully")
+    );
     await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`);
   } catch (error) {
     console.log(error);
